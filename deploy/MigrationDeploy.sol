9b0d37648f4cfe4ea63d833a2fc0e97650c7e67e
telome <>
2025-10-17 14:17:25 +0400

    Fix sol version

--- deploy/MigrationDeploy.sol
+++ deploy/MigrationDeploy.sol
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.22;
 
 import { IManagerBase, NttManager } from "lib/sky-ntt-migration/evm/src/NttManager/NttManager.sol";
 
--- test/Migration.t.sol
+++ test/Migration.t.sol
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.22;
 
 import { DssTest } from "dss-test/DssTest.sol";
 
